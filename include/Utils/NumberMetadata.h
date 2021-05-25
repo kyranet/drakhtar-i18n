7c6f2392aebdfa8ef55c0b1bc101e3f3e593b12c
Pablo Cubells <pablocub@ucm.es>
2021-05-25 12:00:25 +0200

    focusing

---
+++ include/Utils/NumberMetadata.h
@@ -0,0 +1,33 @@
+// Copyright 2021 the Drakhtar authors. All rights reserved. MIT license.
+
+#pragma once
+#include <algorithm>
+#include <string>
+
+// the type of numeric system language
+enum class numberSystem { latn };
+using nSys = numberSystem;
+
+class NumericMetadata {
+ private:
+  // nSys lan_system;
+  std::string decimal;
+  std::string group;
+  std::string list;
+  std::string percentSign;
+  std::string plusSign;
+  std::string minusSign;
+  std::string approximatelySign;
+  std::string exponential;
+  std::string superscriptingExponent;
+  std::string perMille;
+  std::string infinity;
+  std::string nan;
+  std::string timeSeparator;
+
+ public:
+  NumericMetadata(size_t number, numberSystem system) { analice(number); };
+
+ private:
+  void analice(size_t number);
+};
---
+++ src/Utils/NumberMetadata.cpp
@@ -0,0 +1,9 @@
+// Copyright 2021 the Drakhtar authors. All rights reserved. MIT license.
+
+#include "Information/Locale.h"
+#include "Utils/NumericMetadata.h"
+#include
+
+void NumericMetadata::analice(size_t number) {
+  // data map assignation values
+}
