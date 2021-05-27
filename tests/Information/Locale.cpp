7dc2e527b963b8043be5a74451bab3f0796b55df
Antonio Román <kyradiscord@gmail.com>
2021-05-27 19:38:38 +0200

    tests: added tests for Locale

--- tests/Information/Locale.cpp
+++ tests/Information/Locale.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+#include <fstream>
+
 TEST(Locale, non_initialized_state) {
   Locale l{};
   EXPECT_TRUE(l.name().empty());
@@ -15,4 +17,39 @@ TEST(Locale, initialized_state) {
   EXPECT_EQ(l.name(), "English (United States)");
   EXPECT_EQ(l.locale(), "en-US");
   EXPECT_EQ(l.size(), 0);
+}
+
+TEST(Locale, load_with_no_content) {
+  Locale l{};
+  EXPECT_NO_THROW(l.init("en-US"));
+  EXPECT_NO_THROW(l.load());
+  EXPECT_EQ(l.size(), 0);
+}
+
+TEST(Locale, load_with_content) {
+  std::filesystem::path path{"languages"};
+  path.append("fr-FR");
+
+  std::filesystem::create_directory(path);
+
+  const auto meta = path / ".meta.txt";
+  std::ofstream fMeta(meta);
+  fMeta << "NAME=\"Français (France)\"\n";
+  fMeta.close();
+
+  const auto misc = path / "misc.txt";
+  std::ofstream fMisc(misc);
+  fMisc << "HELLO=\"Hello {0}!\"\n";
+  fMisc.close();
+
+  Locale l{};
+  EXPECT_NO_THROW(l.init("fr-FR"));
+  EXPECT_NO_THROW(l.load());
+  EXPECT_EQ(l.size(), 1);
+
+  EXPECT_EQ(l.get("HELLO").run({"Drakhtar"}), "Hello Drakhtar!");
+
+  std::filesystem::remove(misc);
+  std::filesystem::remove(meta);
+  std::filesystem::remove(path);
 }
