33ffc6de861d2a5a16a4026766cca613e06f574b
Pablo Cubells <pablocub@ucm.es>
2021-05-30 20:08:45 +0200

    fix: draft test

---
+++ tests/Utils/NumberMetadata.cpp
@@ -0,0 +1,10 @@
+// #include "Utils/NumberMetadata.h"
+
+// #include <gtest/gtest.h>
+
+// TEST(UtilsNumberMetadata, getNumber) {
+//   EXPECT_EQ(NumberMetadata::zero(), 0);
+//   EXPECT_EQ(NumberMetadata::one(), 1);
+//   EXPECT_EQ(NumberMetadata::two(), 2);
+//   EXPECT_EQ(NumberMetadata::three(), 3);
+// }
